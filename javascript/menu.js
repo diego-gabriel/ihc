814973d48a1b938d99ea5c1fb6a2e8f1a9ca899d
diego-gabriel <diego.gabriel.nd@gmail.com>
2014-12-14 18:20:30 -0400

    hay menu

---
+++ javascript/menu.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+	$('ul.menu li:even').addClass("darker");
+	$('ul.menu li:gt(0)').addClass("left-b");
+	$('ul.menu li:first').addClass("first");
+	$('ul.menu li:last').addClass("last");
+});
