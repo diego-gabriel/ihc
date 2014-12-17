26717b66c8580bf0a34c4a058a54e531c99a28ab
Alison Fernandez Blanco <fernablanco.alison@gmail.com>
2014-12-17 17:21:46 -0400

    mejor jquery

--- javascript/sesion.js
+++ javascript/sesion.js
@@ -1,28 +1,21 @@
 function iniciarSesion(){
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
 	if(typeof(Storage) !== "undefined") {
 		guardarUP(username, password);
 	}
 }
 
 function registrar(){
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
-	var email = document.getElementById("email").value;
-	var nit = document.getElementById("nit").value;
-	var ci = document.getElementById("ci").value;
 	if(typeof(Storage) !== "undefined") {
-		guardarUP(username, password);
-		localStorage.setItem("email", email);
-		localStorage.setItem("nit", nit);
-		localStorage.setItem("ci", ci);
+		guardarUP();
+		localStorage.setItem("email", $("#email").val());
+		localStorage.setItem("nit", $("#nit").val());
+		localStorage.setItem("ci", $("#ci").val());
 	}
 }
 
-function guardarUP(username, password){
-	localStorage.setItem("username", username);
-	localStorage.setItem("password", password);
+function guardarUP(){
+	localStorage.setItem("username", $("#username").val());
+	localStorage.setItem("password", $("#password").val());
 }
 
 
