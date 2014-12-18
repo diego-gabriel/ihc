79a78493351ece67d78fd1b181fb87577bc35703
diego-gabriel <diego.gabriel.nd@gmail.com>
2014-12-18 09:48:13 -0400

    listo! chis?

--- javascript/manejoForm.js
+++ javascript/manejoForm.js
@@ -3,7 +3,7 @@ function borrarForm(){
 }
 
 function borrarSeleccion(confirm_required){
-	if (!confirm_required || confirm("¿esta seguro que quiere borrar los elementos seleccionados?")){	
+	if (!confirm_required || confirm("¿Esta seguro que quiere borrar los elementos seleccionados?")){	
 		var checks = $("input:checked");
 		checks.each(function(index){
 			$(this).parent().parent().fadeOut(500);
